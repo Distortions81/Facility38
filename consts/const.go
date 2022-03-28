f8f7966246fd7e77313484ad176fb277bd3244ac
Distortions81 <carlotto81@gmail.com>
2022-03-28 12:33:04 -0600

    change version

--- consts/const.go
+++ consts/const.go
@@ -4,8 +4,8 @@ const (
 	//Code written by CarlOtto81@gmail.com
 	//MPL-2.0 License
 	DEBUG    = false
-	Version  = "002"        //increment
-	Build    = "03.23.2022" //mmddyyyy
+	Version  = "003"        //increment
+	Build    = "03.28.2022" //mmddyyyy
 	Wasm     = "js"         //Detect wasm/js
 	DataDir  = "data/"
 	GfxDir   = "gfx/"
