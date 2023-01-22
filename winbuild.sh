06dcae20bd85450c571fa9677477ea5991a7c6ab
Distortions81 <carlotto81@gmail.com>
2023-01-22 16:17:54 -0700

    don't inlcude images in winbuild

--- winbuild.sh
+++ winbuild.sh
@@ -1,3 +1,4 @@
 GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o GameTest.exe
 upx --brute GameTest.exe
-zip GameTest.zip GameTest.exe data/gfx/* data/gfx/*/*.png
+#zip GameTest.zip GameTest.exe data/gfx/* data/gfx/*/*.png
+zip GameTest.zip GameTest.exe
