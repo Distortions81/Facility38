095fa140cbd7347090ade3f7b33430a4e3cfbdbf
carlotto81 <carlotto81@gmail.com>
2023-01-23 09:57:08 -0700

    add upx

--- winbuild.sh
+++ winbuild.sh
@@ -1,4 +1,4 @@
-GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o GameTest.exe
-#upx --brute GameTest.exe
+GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-H windowsgui -s -w" -o GameTest.exe
+upx -9 -f GameTest.exe
 #zip GameTest.zip GameTest.exe data/gfx/* data/gfx/*/*.png
 zip GameTest-win64.zip GameTest.exe
