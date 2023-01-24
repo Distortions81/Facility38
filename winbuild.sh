64425894768e63bd76ee58aaf7187d5fe9ac4828
Distortions81 <carlotto81@gmail.com>
2023-01-23 20:47:04 -0700

    sha256

--- winbuild.sh
+++ winbuild.sh
@@ -2,7 +2,7 @@
 #sudo apt install osslsigncode
 rm *.exe *.zip *.upx
 
-GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-H windowsgui -s -w" -o GameTest.exe
-upx -9 GameTest.exe
-osslsigncode sign -certs ~/code-cert/cert-20230123-193538.crt -key ~/code-cert/key-20230123-193538.pem -t http://timestamp.digicert.com -in GameTest.exe -out GameTest-signed.exe
-zip GameTest-win64-signed.zip GameTest-signed.exe data/gfx/* data/gfx/*/*.pngsudo apt install osslsigncode
+GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o GameTest.exe
+#upx -9 GameTest.exe
+osslsigncode sign -certs ~/code-cert/cert-20230123-193538.crt -key ~/code-cert/key-20230123-193538.pem -t http://timestamp.digicert.com -h sha256 -in GameTest.exe -out GameTest-signed.exe
+zip GameTest-win64-signed.zip GameTest-signed.exe data/gfx/* data/gfx/*/*.png
