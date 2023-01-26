03d4ce6827afa02a86ad1c6368faf10769d6f5e3
carlotto81 <carlotto81@gmail.com>
2023-01-26 14:50:51 -0700

    include intro.txt

--- BUILD/linuxbuild.sh
+++ BUILD/linuxbuild.sh
@@ -1,3 +1,3 @@
 GOGC=10 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o GameTest-linux64
-zip -9 GameTest-linux64.zip GameTest-linux64 data/gfx/* data/gfx/*/*.png
+zip -9 GameTest-linux64.zip GameTest-linux64 data/gfx/* data/gfx/*/*.png intro.txt
 rm GameTest-linux64
