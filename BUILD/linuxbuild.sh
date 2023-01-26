e1b1d32770468bd87cc140c3d9e2106785e87cfa
carlotto81 <carlotto81@gmail.com>
2023-01-26 08:06:56 -0700

    cleanup builds

--- BUILD/linuxbuild.sh
+++ BUILD/linuxbuild.sh
@@ -1,4 +1,3 @@
-rm GameTest *.zip
-
-GOGC=10 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o GameTest
-zip -9 GameTest-linux64.zip GameTest data/gfx/* data/gfx/*/*.png
+GOGC=10 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o GameTest-linux64
+zip -9 GameTest-linux64.zip GameTest-linux64 data/gfx/* data/gfx/*/*.png
+rm GameTest-linux64
--- BUILD/winbuild.sh
+++ BUILD/winbuild.sh
@@ -1,8 +1,6 @@
 
 #sudo apt install osslsigncode
-rm *.exe *.zip *.upx
-
-GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o GameTest.exe
-osslsigncode sign -certs ~/code-cert/cert-20230123-193538.crt -key ~/code-cert/key-20230123-193538.pem -t http://timestamp.digicert.com -h sha256 -in GameTest.exe -out GameTest-signed.exe
-zip GameTest-win64-signed.zip GameTest-signed.exe data/gfx/* data/gfx/*/*.png
-rm *.exe
+GOGC=10 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o GameTest-win64-raw.exe
+osslsigncode sign -certs ~/code-cert/cert-20230123-193538.crt -key ~/code-cert/key-20230123-193538.pem -t http://timestamp.digicert.com -h sha256 -in GameTest-win64-raw.exe -out GameTest-win64.exe 
+zip GameTest-win64.zip GameTest-win64.exe data/gfx/* data/gfx/*/*.png
+rm GameTest-win64-raw.exe GameTest-win64.exe
