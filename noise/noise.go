f8a03eede8c1c04d99d23e4f53cb481a8ab5c09e
Distortions81 <carlotto81@gmail.com>
2023-01-26 19:53:27 -0700

    noise working

--- noise/noise.go
+++ noise/noise.go
@@ -2,22 +2,24 @@ package noise
 
 import (
 	"GameTest/consts"
+	"math/rand"
 
 	"github.com/aquilax/go-perlin"
 )
 
 const (
-	alpha       = 2.
-	beta        = 2.
-	n           = 3
-	seed  int64 = 100
+	alpha = 2.
+	beta  = 2.
+	n     = 3
 )
 
 var (
-	per *perlin.Perlin
+	per  *perlin.Perlin
+	seed int64 = 100
 )
 
 func InitPerlin() {
+	seed = rand.Int63()
 	per = perlin.NewPerlin(alpha, beta, n, seed)
 }
 
