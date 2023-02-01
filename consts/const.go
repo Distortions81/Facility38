7b702c1ad6da48597ae84d35f87ab52e7dfe7b1f
carlotto81 <carlotto81@gmail.com>
2023-02-01 14:35:53 -0700

    .

--- consts/const.go
+++ consts/const.go
@@ -11,10 +11,10 @@ const (
 
 	/* Debug */
 	UPSBench    = false
-	LoadTest    = true
+	LoadTest    = false
 	Debug       = false
 	Verbose     = false
-	LogStdOut   = true
+	LogStdOut   = false
 	TestObjects = 1000000 //Make (approx) this number items
 
 	/* Limit numbers of chunks that can be drawn */
