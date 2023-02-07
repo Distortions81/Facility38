3bb23976a94c2321779a7561a20cae5ff071c79d
carlotto81 <carlotto81@gmail.com>
2023-02-07 10:32:41 -0700

    .

--- BUILD/bwasm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm html/main.wasm.gz
-rm html/main.wasm
-
-curTime=`date -u '+%Y%m%d%H%M%S'`
-
-GOGC=10 GOOS=js GOMAXPROCS=1 GOARCH=wasm go build -tags=ebitensinglethread -ldflags="-s -w -X main.buildTime=$curTime -X gv.WASMMode=true" -o html/main.wasm
-gzip -9 html/main.wasm
