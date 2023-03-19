1032314d42d02afdaabbf45da5def9db506bca72
carlotto81 <carlotto81@gmail.com>
2023-03-19 12:13:41 -0600

    belt blocking

--- objects/obj-tock.go
+++ objects/obj-tock.go
@@ -95,7 +95,7 @@ func beltUpdate(obj *world.ObjData) {
 	}
 
 	/* Does the input contain anything? */
-	if obj.Inputs[obj.LastInput].Buf.Amount > 0 {
+	if obj.Inputs[obj.LastInput].Buf.Amount > 0 && obj.Outputs[0].Buf.Amount == 0 {
 		/* Good to go, swap pointers */
 		*obj.Outputs[0].Buf, *obj.Inputs[obj.LastInput].Buf = *obj.Inputs[obj.LastInput].Buf, *obj.Outputs[0].Buf
 	}
