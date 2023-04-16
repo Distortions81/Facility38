2a3934965f638818c1511a1dc62cd29a25547cab
Distortions81 <carlotto81@gmail.com>
2023-04-16 14:07:00 -0600

    .

--- objects/new-tick.go
+++ objects/new-tick.go
@@ -3,29 +3,59 @@ package objects
 import "GameTest/world"
 
 type OffsetData struct {
-	Offset uint64
+	Offset int
 	Ticks  []NewTickEvent
 	Tocks  []NewTickEvent
 }
 
 type NewTickEvent struct {
 	Obj    *world.ObjData
-	Offset uint64
+	Offset uint8
 }
 
 type TickInterval struct {
-	Interval uint64
-	Offsets  []OffsetData
+	Interval   int
+	LastOffset int
+	Offsets    []OffsetData
 }
 
 var TickIntervals []TickInterval
 
-/* TO DO: Add items to these and test */
+/* Init at boot */
+func init() {
+	for _, ot := range WorldObjs {
+		GetInterval(int(ot.TockInterval))
+	}
+}
 
-func NewRunTocks() {
+/* Return interval data, or create it if needed */
+func GetInterval(interval int) int {
+	foundInterval := false
+
+	/* Eventually replace with precalc table */
+	for ipos, inter := range TickIntervals {
+		if inter.Interval == interval {
+			foundInterval = true
+			return ipos
+		}
+	}
+	if !foundInterval {
+		pos := len(TickIntervals)
+		TickIntervals = append(TickIntervals, TickInterval{Interval: interval})
+		return pos
+	}
+	return -1
+}
+
+func AddTock(obj *world.ObjData) {
+	//interval := GetInterval(int(obj.Unique.TypeP.TockInterval))
+
+}
+
+func NewRunTocksST() {
 	for _, ti := range TickIntervals {
 		for _, off := range ti.Offsets {
-			if GameTick%(ti.Interval+off.Offset) == 0 {
+			if GameTick%uint64(ti.Interval+off.Offset) == 0 {
 				for _, tock := range off.Tocks {
 					tock.Obj.Unique.TypeP.UpdateObj(tock.Obj)
 				}
@@ -34,10 +64,10 @@ func NewRunTocks() {
 	}
 }
 
-func NewRunTicks() {
+func NewRunTicksST() {
 	for _, ti := range TickIntervals {
 		for _, off := range ti.Offsets {
-			if GameTick%(ti.Interval+off.Offset) == 0 {
+			if GameTick%uint64(ti.Interval+off.Offset) == 0 {
 				for _, tock := range off.Tocks {
 					tickObj(tock.Obj)
 				}
