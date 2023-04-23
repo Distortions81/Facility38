58dd9107b2b9c3bf30b0d23cc438925cb871d8ea
Distortions81 <carlotto81@gmail.com>
2023-04-23 02:50:00 -0600

    test text

--- window-draw.go
+++ window-draw.go
@@ -1,5 +1,11 @@
 package main
 
-func testWindow(window *WindowData) {
+import (
+	"Facility38/world"
+	"image/color"
+)
 
+func testWindow(window *WindowData) {
+	DrawText("Test", world.BootFont, world.ColorRed, color.Transparent,
+		world.XYf32{X: float32(window.Size.X / 2), Y: float32(window.Size.Y / 2)}, 0, window.Cache, false, false, false)
 }
