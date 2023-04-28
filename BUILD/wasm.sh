cc23d0eb330beed1f40dc70e7eaa684806a06f18
Distortions81 <carlotto81@gmail.com>
2023-04-27 22:43:10 -0600

    incude debug

--- BUILD/wasm.sh
+++ BUILD/wasm.sh
@@ -10,6 +10,6 @@ if [ $# -eq 1 ]; then
   curTime=$1
 fi
 
-GOGC=100 GOOS=js GOMAXPROCS=1 GOARCH=wasm go build -tags=ebitensinglethread -ldflags="-s -w -X main.buildTime=$curTime -X main.NoDebug=true -X main.WASMMode=true" -o BUILD/builds/wasm/main.wasm
+GOGC=100 GOOS=js GOMAXPROCS=1 GOARCH=wasm go build -tags=ebitensinglethread -ldflags="-X main.buildTime=$curTime -X main.NoDebug=true -X main.WASMMode=true" -o BUILD/builds/wasm/main.wasm
 gzip -9 BUILD/builds/wasm/main.wasm
 rm BUILD/builds/wasm/main.wasm
--- BUILD/winbuild.sh
+++ BUILD/winbuild.sh
@@ -10,7 +10,7 @@ if [ $# -eq 1 ]; then
 fi
 
 
-GOGC=100 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w -X main.buildTime=$curTime -X main.NoDebug=true" -o BUILD/builds/Facility38-win64-raw.exe
+GOGC=100 GOOS=windows GOARCH=amd64 go build -ldflags="-X main.buildTime=$curTime -X main.NoDebug=true" -o BUILD/builds/Facility38-win64-raw.exe
 osslsigncode sign -certs ~/code-cert/cert-20230123-193538.crt -key ~/code-cert/key-20230123-193538.pem -t http://timestamp.digicert.com -h sha256 -in BUILD/builds/Facility38-win64-raw.exe -out BUILD/builds/Facility38-$curTime-win64.exe 
 zip BUILD/builds/Facility38-$curTime-win64.zip BUILD/builds/Facility38-$curTime-win64.exe
 rm BUILD/builds/Facility38-win64-raw.exe BUILD/builds/Facility38-$curTime-win64.exe
