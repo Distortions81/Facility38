c72c7e590c1a445664eb7412381eaeb69537f11b
Distortions81 <carlotto81@gmail.com>
2023-05-01 22:47:26 -0600

    comment

--- wasm-fix.go
+++ wasm-fix.go
@@ -3,6 +3,12 @@
 
 package main
 
+/*
+ * This file is a stub for non-wasm platforms.
+ * Without it, some functions would have no
+ * reference and the compile would fail
+ */
+
 func SendBytes(name string, bytes []byte) {
 
 }
