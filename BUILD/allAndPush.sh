f676307996ed764a955ed8774d657cde2d2248bc
Distortions81 <carlotto81@gmail.com>
2023-05-11 05:44:45 -0600

    build script

--- BUILD/allAndPush.sh
+++ BUILD/allAndPush.sh
@@ -9,4 +9,6 @@ bash BUILD/linuxbuild.sh $curTime
 bash BUILD/wasm.sh $curTime
 
 7z a -t7z BUILD/builds/code-ref/$versionString.7z *.go */*.go
-echo "Build of '$versionString' complete."
+echo "Build of '$versionString' complete, pushing build."
+
+../BuildUtilF38/BuildUtilF3 -build="$versionString" -path="../Facility38/BUILD/builds/" -local
